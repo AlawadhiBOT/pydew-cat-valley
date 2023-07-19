0ece53ace880c9613740fc7f82d57b04038c3f97
OmarAlawadhi <90068631+AlawadhiBOT@users.noreply.github.com>
2023-07-20 02:18:29 +0400

    More map updates (imgs stuff).

--- data/Tilesets/Barn Objects.tsx
+++ data/Tilesets/Barn Objects.tsx
@@ -1,31 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.1" name="Barn Objects" tilewidth="256" tileheight="208" tilecount="9" columns="0">
- <grid orientation="orthogonal" width="1" height="1"/>
- <tile id="0">
-  <image width="256" height="208" source="../../graphics/objects/big_haypatch.png"/>
- </tile>
- <tile id="1">
-  <image width="44" height="40" source="../../graphics/objects/haypatch.png"/>
- </tile>
- <tile id="2">
-  <image width="112" height="52" source="../../graphics/objects/big_haystack.png"/>
- </tile>
- <tile id="3">
-  <image width="64" height="52" source="../../graphics/objects/haystack.png"/>
- </tile>
- <tile id="4">
-  <image width="96" height="100" source="../../graphics/objects/boxes.png"/>
- </tile>
- <tile id="5">
-  <image width="64" height="60" source="../../graphics/objects/box.png"/>
- </tile>
- <tile id="7">
-  <image width="128" height="63" source="../../graphics/objects/full_water_tray.png"/>
- </tile>
- <tile id="8">
-  <image width="128" height="64" source="../../graphics/objects/empty_water_tray.png"/>
- </tile>
- <tile id="9">
-  <image width="128" height="64" source="../../graphics/objects/water_tray.png"/>
- </tile>
+<tileset version="1.10" tiledversion="1.10.1" name="Barn Objects" tilewidth="64" tileheight="64" tilecount="36" columns="6">
+ <image source="../../graphics/environment/Barn Decoration.png" width="384" height="384"/>
 </tileset>
