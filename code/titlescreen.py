d9c3c91fd4033f908ae31593aa5e8b803e112f06
OmarAlawadhi <90068631+AlawadhiBOT@users.noreply.github.com>
2024-05-05 04:39:48 -0400

    titlescreen.py update I've been holding since summer...

--- code/titlescreen.py
+++ code/titlescreen.py
@@ -1,5 +1,5 @@
 from code.settings import *
-from support import *
+from code.support import *
 
 
 class TitleScreen:
@@ -8,13 +8,80 @@ class TitleScreen:
         Class to handle title screen, and possibly logins.
         """
         norm = os.path.normpath
-        self.title_path = "graphics/title"
-        self.background_image = pygame.image.load(norm(CURR_PATH +
-                                                       f"{self.title_path}/"
-                                                       f"screens"
+        self.display_surface = pygame.display.get_surface()
+
+        self.title_path = CURR_PATH + "/graphics/title"
+        self.background_image = pygame.image.load(norm(f"{self.title_path}/"
+                                                       f"screens/"
                                                        "background-for-"
                                                        "title-screen.png")
                                                   ).convert_alpha()
-        self.buttons = import_folder_dict2(f"{self.title_path}/buttons/")
+        self.background_image_login = (
+            pygame.image.load(norm(f"{self.title_path}/screens/background-for-"
+                                   "title-screen.png")
+                              ).convert_alpha())
+        infoObject = pygame.display.Info()
+        width, height = infoObject.current_w, infoObject.current_h
+        self.background_image = pygame.transform.scale(self.background_image,
+                                                       (width, height))
+        self.background_image_rect = self.background_image.get_rect()
 
+        self.buttons = import_folder_dict(norm(f"{self.title_path}/buttons/"))
+        self.buttons_rects = {key: value.get_rect() for key, value in
+                              self.buttons.items()}
+        self.active_buttons = {"play": "play_button",
+                               "login": "login_button"}
+        self.setup()
 
+        self.status = "title"
+
+    def setup(self):
+        """Fixes position of some things"""
+        self.buttons_rects["login_button"].midbottom = \
+            OVERLAY_POSITIONS["inven"]
+        self.buttons_rects["login_button_pressed"].midbottom = \
+            self.buttons_rects["login_button"].midbottom
+
+        self.buttons_rects["play_button"].midbottom = \
+            self.buttons_rects["login_button"].midtop - Vector2(0, 10)
+        self.buttons_rects["play_button_pressed"].midbottom = \
+            self.buttons_rects["play_button"].midbottom
+
+    def display_buttons(self):
+        for val in self.active_buttons.values():
+            self.display_surface.blit(self.buttons[val],
+                                      self.buttons_rects[val])
+
+    def input(self):
+        keys = pygame.key.get_pressed()
+
+        mousex, mousey = pygame.mouse.get_pos()
+
+        for key, button in self.active_buttons.items():
+            if pygame.mouse.get_pressed(num_buttons=3)[0] and \
+                    self.buttons_rects[button].collidepoint(mousex, mousey):
+                if "pressed" not in button:
+                    self.status = "".join(button.split("_")[:-1])
+                    print(self.status)
+                    self.active_buttons[key] = button + "_pressed"
+            else:
+                if "_pressed" in button:
+                    new_button = button[0:button.rfind("_pressed")]
+                else:
+                    new_button = button
+                self.active_buttons[key] = new_button
+
+    def run(self, dt):
+        """
+        Runs the title page
+        """
+        self.display_surface.fill("black")
+
+        if self.status == "title":
+            self.display_surface.blit(self.background_image,
+                                      self.background_image_rect)
+        if self.status == "play":
+            pass
+
+        self.display_buttons()
+        self.input()
